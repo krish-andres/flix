8c890a5415897100e21fd5f07521ca2fff42cf65
Krish <krish.andres@gmail.com>
2014-09-15 16:09:39 -0700

    Add validations to movie model

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -32,4 +32,113 @@ describe "A movie" do
 
     expect(Movie.released).to eq([movie3, movie2, movie1])
   end
+
+  it "requires a title" do
+    movie = Movie.new(title: "")
+
+    movie.valid?
+
+    expect(movie.errors[:title].any?).to eq(true)
+  end
+
+  it "requires a description" do
+    movie = Movie.new(description: "")
+
+    movie.valid?
+
+    expect(movie.errors[:description].any?).to eq(true)
+  end
+
+  it "requires a release date" do
+    movie = Movie.new(released_on: "")
+
+    movie.valid?
+
+    expect(movie.errors[:released_on].any?).to eq(true)
+  end
+
+  it "requires a duration" do
+    movie = Movie.new(duration: "")
+
+    movie.valid?
+
+    expect(movie.errors[:duration].any?).to eq(true)
+  end
+
+  it "requires a description over 24 characters" do
+    movie = Movie.new(description: "X" * 24)
+
+    movie.valid?
+
+    expect(movie.errors[:description].any?).to eq(true)
+  end
+
+  it "accepts a $0 total gross" do
+    movie = Movie.new(total_gross: 0.00)
+
+    movie.valid?
+    
+    expect(movie.errors[:total_gross].any?).to eq(false)
+  end
+
+  it "accepts a positive total gross" do
+    movie = Movie.new(total_gross: 1000000.00)
+
+    movie.valid?
+    
+    expect(movie.errors[:total_gross].any?).to eq(false)
+  end
+
+  it "rejects a negative total gross" do
+    movie = Movie.new(total_gross: -1000000.00)
+
+    movie.valid?
+
+    expect(movie.errors[:total_gross].any?).to eq(true)
+  end
+
+  it "accepts properly formatted image file names" do
+    file_names = %w[e.png movie.png movie.jpg movie.gif MOVIE.gif]
+    file_names.each do |file|
+      movie = Movie.new(image_file_name: file)
+      movie.valid?
+      expect(movie.errors[:image_file_name].any?).to eq(false)
+    end
+  end
+
+  it "rejects properly formatted image file names" do
+    file_names = %w[movie .jpg .png .gif movie.pdf movie.doc]
+    file_names.each do |file|
+      movie = Movie.new(image_file_name: file)
+      movie.valid?
+      expect(movie.errors[:image_file_name].any?).to eq(true)
+    end
+  end
+
+  it "accepts any rating that is in an approved list" do
+    ratings = %w[G PG PG-13 R NC-17]
+    ratings.each do |rating|
+      movie = Movie.new(rating: rating)
+      movie.valid?
+      expect(movie.errors[:rating].any?).to eq(false)
+    end
+  end
+
+  it "rejects any rating that is not in an approved list" do
+    ratings = %w[R-13 R-16 R-18 R-21]
+    ratings.each do |rating|
+      movie = Movie.new(rating: rating)
+      movie.valid?
+      expect(movie.errors[:rating].any?).to eq(true)
+    end
+  end
+
+  it "is valid with example attributes" do
+    movie = Movie.new(movie_attributes)
+
+    expect(movie.valid?).to eq(true)
+  end
+
+
+
 end
