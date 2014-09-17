f4d636ab356e3ef10a010266bbe1bbd070d2eea3
Krish <krish.andres@gmail.com>
2014-09-16 19:35:45 -0700

    Nest reviews resource and add reviews index page

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,13 @@
 class ReviewsController < ApplicationController
+  before_action :find_movie
+
+  def index
+    @reviews = @movie.reviews
+  end
+
+
+  private
+  def find_movie
+    @movie = Movie.find(params[:movie_id])
+  end
 end
